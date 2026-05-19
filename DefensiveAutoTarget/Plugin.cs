67a9d305490e550f2b5ebefb176a5cf5ede3e678
Josiah South <josiahdsouth@gmail.com>
2026-05-18 20:27:37 -0400

    Demonstrate client-side bypass of defensive auto target gating

--- DefensiveAutoTarget/Plugin.cs
+++ DefensiveAutoTarget/Plugin.cs
@@ -1,12 +1,9 @@
 using BepInEx;
 using BepInEx.Configuration;
 using HarmonyLib;
-using NuclearOption.Networking;
-using NuclearOption.Networking.Lobbies;
-using Steamworks;
-using System;
-using System.Collections;
 using UnityEngine;
+
+#error SECURITY DEMO ONLY - intentionally non-compilable proof of client-side bypassability.
 
 namespace DefensiveAutoTarget
 {
@@ -14,13 +11,8 @@ namespace DefensiveAutoTarget
     public class Plugin : BaseUnityPlugin
     {
         public const string PluginGUID = "com.defensiveautotarget";
-        private const string LobbyDataKey = "dat_mod";
-        private const string MemberDataKey = "dat_mod";
-        private const string ModVersion = "0.5.1";
 
         public static ConfigEntry<KeyCode> AutoTargetKey;
-        public static bool ModEnabled;
-        public static bool IsSinglePlayer;
         public static Plugin Instance;
         private Harmony _harmony;
 
@@ -39,56 +31,6 @@ namespace DefensiveAutoTarget
             _harmony.PatchAll();
 
             Logger.LogInfo("Defensive Auto Target loaded.");
-        }
-
-        private static CSteamID GetCurrentLobby()
-        {
-            try
-            {
-                if (SteamLobby.instance == null)
-                    return new CSteamID();
-
-                var hostedLobby = (HostedLobbyInstance)AccessTools
-                    .Field(typeof(SteamLobby), "_hostedLobby")
-                    .GetValue(SteamLobby.instance);
-
-                if (hostedLobby.IsValid)
-                    return hostedLobby.Id;
-
-                var joinedLobby = (LobbyInstance)AccessTools
-                    .Field(typeof(SteamLobby), "_joinedLobby")
-                    .GetValue(SteamLobby.instance);
-
-                if (joinedLobby is PlayerLobbyInstance playerLobby)
-                {
-                    var prop = AccessTools.Property(typeof(PlayerLobbyInstance), "LobbyId");
-                    if (prop != null)
-                    {
-                        var id = (CSteamID)prop.GetValue(playerLobby);
-                        if (id.m_SteamID != 0UL)
-                            return id;
-                    }
-                }
-            }
-            catch (Exception) { }
-
-            return new CSteamID();
-        }
-
-        /// <summary>
-        /// Check if every member in the lobby has set their dat_mod member data.
-        /// </summary>
-        private static bool AllLobbyMembersHaveMod(CSteamID lobbyId)
-        {
-            int count = SteamMatchmaking.GetNumLobbyMembers(lobbyId);
-            for (int i = 0; i < count; i++)
-            {
-                CSteamID member = SteamMatchmaking.GetLobbyMemberByIndex(lobbyId, i);
-                string val = SteamMatchmaking.GetLobbyMemberData(lobbyId, member, MemberDataKey);
-                if (string.IsNullOrEmpty(val))
-                    return false;
-            }
-            return count > 0;
         }
 
         public static void AutoTargetNearestMissile(Aircraft aircraft)
@@ -113,129 +55,15 @@ namespace DefensiveAutoTarget
             combatHud.SelectUnit(missile);
         }
 
-        // Single player: enable immediately, no lobby needed.
-        // Multiplayer host: advertise in lobby data, set own member data, start polling.
-        [HarmonyPatch(typeof(NetworkManagerNuclearOption), "StartHostAsync")]
-        private class HostStartPatch
-        {
-            [HarmonyPostfix]
-            private static void Postfix(NetworkManagerNuclearOption __instance, HostOptions options)
-            {
-                IsSinglePlayer = options.SocketType == SocketType.Offline;
-
-                if (IsSinglePlayer)
-                {
-                    ModEnabled = true;
-                    return;
-                }
-
-                // Multiplayer host: set lobby data + own member data, then poll
-                ModEnabled = false;
-                __instance.StartCoroutine(HostSetup());
-            }
-
-            private static IEnumerator HostSetup()
-            {
-                for (int i = 0; i < 10; i++)
-                {
-                    yield return new WaitForSeconds(1f);
-                    var lobbyId = GetCurrentLobby();
-                    if (lobbyId.m_SteamID != 0UL)
-                    {
-                        SteamMatchmaking.SetLobbyData(lobbyId, LobbyDataKey, ModVersion);
-                        SteamMatchmaking.SetLobbyMemberData(lobbyId, MemberDataKey, ModVersion);
-                        ModEnabled = AllLobbyMembersHaveMod(lobbyId);
-                        yield break;
-                    }
-                }
-            }
-        }
-
-        // Client joining: set own member data so host and others can see it.
-        [HarmonyPatch(typeof(SteamLobby), "TryJoinLobby")]
-        private class ClientJoinPatch
-        {
-            [HarmonyPostfix]
-            private static void Postfix()
-            {
-                IsSinglePlayer = false;
-                ModEnabled = false;
-                if (Instance != null)
-                    Instance.StartCoroutine(ClientSetup());
-            }
-
-            private static IEnumerator ClientSetup()
-            {
-                for (int i = 0; i < 10; i++)
-                {
-                    yield return new WaitForSeconds(1f);
-                    var lobbyId = GetCurrentLobby();
-                    if (lobbyId.m_SteamID != 0UL)
-                    {
-                        // Advertise that we have the mod
-                        SteamMatchmaking.SetLobbyMemberData(lobbyId, MemberDataKey, ModVersion);
-
-                        // Check if host has the mod (lobby data)
-                        string hostVal = SteamMatchmaking.GetLobbyData(lobbyId, LobbyDataKey);
-                        if (string.IsNullOrEmpty(hostVal))
-                        {
-                            ModEnabled = false;
-                            yield break;
-                        }
-
-                        // Check all members
-                        ModEnabled = AllLobbyMembersHaveMod(lobbyId);
-                        yield break;
-                    }
-                }
-            }
-        }
-
-        // Re-check whenever a player spawns (joins mid-game).
-        // This runs on the host only (SpawnCharacter is server-side).
-        [HarmonyPatch(typeof(NetworkManagerNuclearOption), "SpawnCharacter")]
-        private class PlayerSpawnPatch
-        {
-            [HarmonyPostfix]
-            private static void Postfix()
-            {
-                if (IsSinglePlayer)
-                    return;
-                if (Instance != null)
-                    Instance.StartCoroutine(RecheckAfterDelay());
-            }
-
-            private static IEnumerator RecheckAfterDelay()
-            {
-                // Give the new player time to set their member data
-                yield return new WaitForSeconds(5f);
-                var lobbyId = GetCurrentLobby();
-                if (lobbyId.m_SteamID != 0UL)
-                    ModEnabled = AllLobbyMembersHaveMod(lobbyId);
-            }
-        }
-
-        // Reset on disconnect.
-        [HarmonyPatch(typeof(NetworkManagerNuclearOption), "Stop")]
-        private class ResetOnDisconnectPatch
-        {
-            [HarmonyPostfix]
-            private static void Postfix()
-            {
-                ModEnabled = false;
-                IsSinglePlayer = false;
-            }
-        }
-
-        // Input: only fire if mod is enabled (single player or all players have it).
+        // Security demonstration:
+        // The removed lobby/member data checks were self-attested client metadata.
+        // They never provided server-side authorization for this local input hook.
         [HarmonyPatch(typeof(PilotPlayerState), "PlayerControls")]
         private class AutoTargetMissilePatch
         {
             [HarmonyPostfix]
             private static void Postfix()
             {
-                if (!ModEnabled)
-                    return;
                 if (AutoTargetKey.Value == KeyCode.None)
                     return;
                 if (!Input.GetKeyDown(AutoTargetKey.Value))
